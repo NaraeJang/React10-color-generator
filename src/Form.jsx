998e2d751efe84b05aa45c4e9e66059637094ef1
NaraeJang <j.naaaarae@gmail.com>
2023-09-26 16:06:55 -0400

    form structure

---
+++ src/Form.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [color, setColor] = useState("");
+
+  const handleSubmit = (e) => {};
+  return (
+    <section className="container">
+      <h4>color generator</h4>
+      <form onSubmit={handleSubmit} className="color-form">
+        <input
+          type="text"
+          value={color}
+          placeholder="#f15025"
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
+        <button type="submit" className="btn" style={{ background: color }}>
+          submit
+        </button>
+      </form>
+    </section>
+  );
+};
+export default Form;
